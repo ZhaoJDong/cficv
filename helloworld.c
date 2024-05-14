1e24f045b5aae44bba710e83a47b2e3228e27c2b
DZJ <dzjupjust@163.com>
2024-05-14 09:42:56 +0800

    add helloworld

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: DZJ
+ * @Date: 2024-05-14 09:35:50
+ * @LastEditors: DZJ
+ * @LastEditTime: 2024-05-14 09:37:42
+ * @FilePath: \cficv\helloworld.c
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+ */
+#include <stdio.h>
+
+int mian(int argc, char** argv)
+{
+    printf("helloworld!");
+}
